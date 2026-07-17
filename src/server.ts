7ffb4fd9c155ad1e0a56a280653a8ffefb6a7abd
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-17 22:10:55 +0600

    server initialization

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+import "dotenv/config";
+
+
+const PORT   = process.env.PORT || 5000;
+
+async function main() {
+    try {
+        // await prisma.$connect();
+        console.log("Connected to the database successfully");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+            
+        })
+        
+    }catch (error) {
+        console.log("Error while starting the server:",error);
+        // await prisma.$disconnect(); // Disconnect from the database if there is an error
+        process.exit(1); 
+        
+    }
+    
+}
+
+main();
